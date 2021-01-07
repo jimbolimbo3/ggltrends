df8449d73d58527945c9e075611845908a73aaa9
Luigi Sirito <jimbolimbo4@gmail.com>
2021-01-07 10:20:48 +0100

    Update trends.py

--- trends.py
+++ trends.py
@@ -1,26 +1,37 @@
+# -*- coding: utf-8 -*-
+
+
 import pandas as pd
+
 from pytrends.request import TrendReq
-
+from pprint import pprint
+import json
 pytrend = TrendReq()
-# Create payload and capture API tokens. Only needed for interest_over_time(), interest_by_region() &related_queries()
-#trendy_keywords = ('pijama')
-kw_list = ['pijama', 'kot pantolon', 'sapka', 'panduf', 'bralet']
-kw = kw_list
-pytrend.build_payload(
-    (kw_list), timeframe='2020-01-01 2020-12-24', geo="TR", cat=18)
-# Related Queries, returns a dictionary of dataframes
-related_queries_dict = pytrend.related_queries()
-print(related_queries_dict)
-# for rising related queries
-related_queries_rising = related_queries_dict.get("pijama").get('rising')
-# for top related queries
-related_queries_top = related_queries_dict.get("pijama").get('top')
-print('**************** RISING RELATED KEYWORDS **************')
-print(related_queries_rising)
-print('**************** TOP RELATED KEYWORDS *******************')
-print(related_queries_top)
 
-for i in kw:
-    print(related_queries_rising)
-    print(related_queries_top)
-related_queries_top.to_csv(r'C:\Users\JJimbo\Desktop\GE.csv', index=False)
+
+product_list = ["sweatshirt", "mont", "pijama"]
+gender_list = ["erkek", "kadın", "çocuk"]
+product_matrix = []
+kw_list = ['sweatshirt', 'kadın sweatshirt', 'erkek sweatshirt', 'çocuk sweatshirt', 'mont',
+           'kadın mont', 'erkek mont', 'çocuk mont', 'pijama', 'kadın pijama', 'erkek pijama', 'çocuk pijama']
+
+for i in range(0, len(product_list)):
+    for j in range(0, len(gender_list)):
+        product_matrix.append(gender_list[j]+" " + product_list[i])
+print(product_matrix)
+
+#kw = kw_list
+
+###########################################
+
+dataframes = []
+
+for i in range(0, len(product_matrix), 4):
+    kw = product_matrix[i:i+4]
+    pytrend.build_payload(kw, timeframe='now 7-d', geo="TR", cat='18')
+    related_queries_dict = pytrend.related_queries()
+    for key in related_queries_dict:
+        df = related_queries_dict.get(key).get('rising')
+        df['kw'] = key
+        dataframes.append(df)
+        df.to_csv(r'C:\Users\JJimbo\Desktop\GE.csv', index=False)
